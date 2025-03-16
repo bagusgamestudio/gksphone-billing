6d550664fe303996b01c03663c4ecf8943559c95
ZHOURA-24 <zhourateam@gmail.com>
2025-03-16 19:53:13 +0700

    feat: add Lua 5.4 support &  add oxmysql

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version "cerulean"
 game "gta5"
+lua54 'yes'
 
 title "GKSPHONE - APP Template"
 description "APP Template GKSPHONE"
@@ -9,8 +10,10 @@ ui_page 'ui/index.html'
 
 shared_script "@ox_lib/init.lua"
 client_script "client/*.lua"
-server_script "server/*.lua"
-
+server_scripts {
+    "@oxmysql/lib/MySQL.lua",
+    "server/*.lua"
+}
 files {
     "ui/index.html",
     "ui/js/*.js",
